c70515c0b58f9d251ffb175ae45d98f631e91556
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-06-30 20:39:10 +0200

    Update LeoCorpLibrary.Designer.cs

--- LABS Experiences/LABS Experiences/Forms/LeoCorpLibrary.Designer.cs
+++ LABS Experiences/LABS Experiences/Forms/LeoCorpLibrary.Designer.cs
@@ -36,6 +36,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -126,12 +127,27 @@
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(31, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(189, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ListViewSaveLoad";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // LeoCorpLibrary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(640, 433);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -158,5 +174,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
