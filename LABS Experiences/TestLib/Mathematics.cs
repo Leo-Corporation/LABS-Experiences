16be708ef10d1678904434c0cb0ffc4ad186da30
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-08 11:57:27 +0200

    Génération

---
+++ LABS Experiences/TestLib/Mathematics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    public class Mathematics
+    {
+        public static int IntSum(params int[] numbers)
+        {
+            int res = 0;
+            foreach (int number in numbers)
+            {
+                res += number;
+            }
+            return res;
+        }
+    }
+}
