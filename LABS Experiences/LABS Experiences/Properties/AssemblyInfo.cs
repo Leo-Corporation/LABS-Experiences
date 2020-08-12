a1e69ff947b921adebf541a6484997d47c0457be
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-12 13:45:48 +0200

    Update AssemblyInfo.cs

--- LABS Experiences/LABS Experiences/Properties/AssemblyInfo.cs
+++ LABS Experiences/LABS Experiences/Properties/AssemblyInfo.cs
@@ -1,18 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Les informations générales relatives à un assembly dépendent de
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("LABS Experiences")]
-[assembly: AssemblyDescription("L'endroit où sont rassemblées nos expériences")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Léo Corporation")]
-[assembly: AssemblyProduct("LABS Experiences")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("Léo Corporation")]
-[assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
@@ -21,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("40ad7618-0b10-45bd-8c46-241331b68b21")]
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
-// en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.2008")]
-[assembly: AssemblyFileVersion("1.4.0.2008")]
