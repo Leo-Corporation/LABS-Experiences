b2cbed42ebe272a2807e9165ae8b563e1e0e48af
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-10-30 11:30:52 +0200

    Version 1.15.0.2110

--- LABS Experiences/LABS Experiences/Forms/AboutForm.Designer.cs
+++ LABS Experiences/LABS Experiences/Forms/AboutForm.Designer.cs
@@ -62,10 +62,10 @@ namespace LABS_Experiences.Forms
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(235, 17);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+			this.pictureBox1.Location = new System.Drawing.Point(206, 13);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(160, 184);
+			this.pictureBox1.Size = new System.Drawing.Size(140, 138);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox1.TabIndex = 4;
 			this.pictureBox1.TabStop = false;
@@ -74,21 +74,21 @@ namespace LABS_Experiences.Forms
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.label2.Location = new System.Drawing.Point(241, 248);
-			this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label2.Location = new System.Drawing.Point(211, 186);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(155, 23);
+			this.label2.Size = new System.Drawing.Size(131, 19);
 			this.label2.TabIndex = 2;
-			this.label2.Text = "Version 1.14.0.2108";
+			this.label2.Text = "Version 1.15.0.2110";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-			this.label1.Location = new System.Drawing.Point(214, 207);
-			this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label1.Location = new System.Drawing.Point(187, 155);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(224, 35);
+			this.label1.Size = new System.Drawing.Size(178, 28);
 			this.label1.TabIndex = 3;
 			this.label1.Text = "LABS Experiences";
 			// 
@@ -97,10 +97,10 @@ namespace LABS_Experiences.Forms
 			this.linkLabel1.AutoSize = true;
 			this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
 			this.linkLabel1.LinkColor = System.Drawing.Color.MediumSlateBlue;
-			this.linkLabel1.Location = new System.Drawing.Point(279, 280);
-			this.linkLabel1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.linkLabel1.Location = new System.Drawing.Point(244, 210);
+			this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(79, 28);
+			this.linkLabel1.Size = new System.Drawing.Size(64, 21);
 			this.linkLabel1.TabIndex = 5;
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "GitHub";
@@ -108,17 +108,17 @@ namespace LABS_Experiences.Forms
 			// 
 			// AboutForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
-			this.ClientSize = new System.Drawing.Size(631, 327);
+			this.ClientSize = new System.Drawing.Size(552, 245);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.label2);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
 			this.MaximizeBox = false;
 			this.Name = "AboutForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
